eb652446256559f72d02cc6c5ef2b10b1b5e7b36
NetAcad DEVASC <changeme@example.com>
2024-06-27 18:23:00 +0000

    Prueba ET

---
+++ datoss.py
@@ -0,0 +1,12 @@
+# Solicitar datos al usuario
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+sede = input("Ingrese su sede: ")
+
+# Imprimir los datos ingresados
+print("\nDatos ingresados:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Edad:", edad)
+print("Sede:", sede)
